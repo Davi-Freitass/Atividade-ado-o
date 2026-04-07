6a869d8b1b60a4c016d23edd57e54299c6c20df5
Davi-Freitass <davi_f_freitas@estudante.sesisenai.org.br>
2026-04-07 11:45:25 -0300

    final

--- script.js
+++ script.js
@@ -3,42 +3,55 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
     e.preventDefault();
 
     let nome = document.getElementById("nome").value;
+
     let email = document.getElementById("email").value;
+
     let telefone = document.getElementById("telefone").value;
+
     let cpf = document.getElementById("cpf").value;
+
     let idade = parseInt(document.getElementById("idade").value);
+
     let cidade = document.getElementById("cidade").value;
+
     let moradia = document.getElementById("moradia").value;
+
     let quintal = document.getElementById("quintal").value;
+
     let pet = document.getElementById("pet").value;
+
     let horas = parseInt(document.getElementById("horas").value);
+
     let motivo = document.getElementById("motivo").value;
+
     let termo = document.getElementById("termo").checked;
+
     let cpfsExistentes = ["12345678900", "11122233344"];
     let condicao = document.getElementById("condicao")?.value;
-    let decisao = document.getElementById("decisao")?.value;
-    let espaco = document.getElementById("espaco")?.value;
-    let motivosInvalidos = ["quero", "porque sim"];
 
-    if (nome.length < 3) return alert("Nome Inválido");
+    let decisao = document.getElementById("decisao")?.value;
+
+    let espaco = document.getElementById("espaco")?.value;
+
+    if (nome.length < 3) return alert("Nome muito curto");
     if (!email.includes("@")) return alert("Email inválido");
     if (telefone.length < 10) return alert("Telefone inválido");
     if (cpf === "") return alert("CPF é obrigatório");
     if (cpfsExistentes.includes(cpf)) return alert("CPF já está cadastrado");
     if (idade < 18) return alert("Você deve ser maior de idade");
-    if (cidade === "") return alert("Cidade obrigatória");
-    if (moradia === "") return alert("Selecione a moradia");
-    if (quintal === "") return alert("Informe sobre o quintal");
-    if (pet === "") return alert("Informe se já teve pet");
-    if (isNaN(horas)) return alert("Horas inválidas");
-    if (motivo.length < 10) return alert("Motivo muito curto");
-    if (!termo) return alert("Aceite o termo");
+    if (cidade === "") return alert("Por favor informe a cidade");
+    if (moradia === "") return alert("Selecione o tipo de moradia");
+    if (quintal === "") return alert("Informe se voce tem quintal");
+    if (pet === "") return alert("Informação obrigatoria");
+    if (isNaN(horas)) return alert("tempo invalido");
+    if (motivo.length < 10) return alert("Informe um motivo melhor");
+    if (!termo) return alert("Obrigatório aceitar os termos para adoção");
 
     if (moradia === "apartamento" && quintal === "sim") {
-        return alert("Apartamento não pode ter quintal");
+        return alert("Apartamento não tem quintal");
     }
     if (horas > 8) {
-        alert("O animal ficará muito tempo sozinho!");
+        alert("O animal não pode ficar muito tempo sozinho!");
     }
     if (pet === "nao") {
         alert("A ONG poderá acompanhar sua adaptação com o pet.");
@@ -74,6 +87,6 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
         Horas sozinho: ${horas}
         Motivo: ${motivo}
         `);
-    formData.requestFullscreen();
+    formData.requestFullscreen(); 
 
 });
